dd94ede1293bc51583c9514981b4d4dcc19a733c
MarcoLGP <marcolucagp@gmail.com>
2023-02-06 15:48:09 -0300

    update

--- src/Home/UsersList.tsx
+++ src/Home/UsersList.tsx
@@ -1,6 +1,6 @@
 import axios, { AxiosResponse } from "axios";
-import { useState } from "react";
-import {FlatList, Image, StyleSheet, Text, View} from "react-native";
+import React, { Dispatch, useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 type usersProps = {
     id: number;
@@ -9,14 +9,19 @@ type usersProps = {
     password: string;
 };
 
+type getUserProps = {
+    setUsers: Dispatch<React.SetStateAction<usersProps[] | undefined>>;
+    setLoading: Dispatch<React.SetStateAction<boolean>>;
+}
+
 const handleUser = (item: usersProps, index: number): JSX.Element => {
     return (
         <View key={index} style={styles.listUsersItemContainer}>
             <Image resizeMode="contain" source={require("../../assets/images/user_img.png")} style={styles.listUserImage} />
             <View style={styles.listRowInfoContainer}>
                 <View style={styles.listRowInfoContent}>
-                <Text style={styles.listUserNameFont}>{item.name}</Text>
-                    <Text>{item.password}</Text>
+                    <Text style={styles.listUserNameFont}>{item.name}</Text>
+                    <Text>{item.email}</Text>
                 </View>
                 <Text style={styles.listUserIdFont}>{item.id}</Text>
             </View>
@@ -24,26 +29,62 @@ const handleUser = (item: usersProps, index: number): JSX.Element => {
     );
 }
 
+const handleEmptyUser = (): JSX.Element => {
+    return (
+        <View style={styles.listUsersItemContainer}>
+            <Image resizeMode="contain" source={require("../../assets/images/user_img.png")} style={styles.listUserImage} />
+            <View style={styles.listRowInfoContainer}>
+                <Text style={[styles.listUserNameFont, { textAlign: "center", width: "100%" }]}>Sem usuários cadastrados</Text>
+            </View>
+        </View>
+    )
+}
+
+const footerUsersList = ({ setLoading, setUsers }: getUserProps): JSX.Element => {
+    return (
+        <View style={styles.footerUsersListContainer}>
+            <TouchableOpacity onPress={() => getUsers({ setLoading, setUsers })} style={styles.footerUsersListBtn}>
+                <Text style={styles.footerUsersListBtnFont}>Atualizar</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const getUsers = ({ setLoading, setUsers }: getUserProps): void => {
+    setLoading(true);
+    axios.get("https://signsystemapi.bsite.net/user/getAll")
+        .then((res: AxiosResponse) => {
+            setLoading(false);
+            setUsers(res.data);
+        });
+}
+
 const UsersList = (): JSX.Element => {
 
     const [users, setUsers] = useState<usersProps[]>();
+    const [loading, setLoading] = useState<boolean>(false);
 
-    axios.get("http://192.168.0.110:5051/user/getAll")
-        .then((res: AxiosResponse) => {
-            setUsers(res.data);
-        });
+    useEffect((): void => {
+        getUsers({ setUsers, setLoading });
+    }, []);
 
     return (
         <View style={styles.container}>
             <Image resizeMode="contain" source={require("../../assets/images/SignSystemLogo.png")} style={styles.imageLogoContainer} />
             <Text style={styles.usersFont}>Usuários</Text>
-            <FlatList
-                showsVerticalScrollIndicator={false}
-                contentContainerStyle={styles.listUsersContentContainer}
-                style={styles.listUsersContainer}
-                data={users}
-                renderItem={({ item, index }) => handleUser(item, index)}
-            />
+            {loading ?
+                <View style={styles.loadingUsersContainer}>
+                    <Text style={styles.loadingUsersFont}>Carregando usuários...</Text>
+                    <ActivityIndicator style={styles.loadingIndicator} />
+                </View> : <FlatList
+                    showsVerticalScrollIndicator={false}
+                    ListEmptyComponent={handleEmptyUser}
+                    ListFooterComponent={footerUsersList({ setLoading, setUsers })}
+                    contentContainerStyle={styles.listUsersContentContainer}
+                    style={styles.listUsersContainer}
+                    data={users}
+                    renderItem={({ item, index }) => handleUser(item, index)}
+                />}
         </View>
     );
 }
@@ -53,7 +94,6 @@ const styles = StyleSheet.create({
         alignItems: "center",
         height: "100%",
         width: "100%",
-        justifyContent: "center"
     },
     imageLogoContainer: {
         height: 230,
@@ -67,7 +107,7 @@ const styles = StyleSheet.create({
         marginTop: -15
     },
     listUsersContainer: {
-        marginTop: 15, 
+        marginTop: 15,
         width: "92%"
     },
     listUsersContentContainer: {
@@ -86,30 +126,53 @@ const styles = StyleSheet.create({
         elevation: 1
     },
     listUserImage: {
-        height: 60, 
+        height: 60,
         width: 60
     },
     listRowInfoContainer: {
-        width: "76.5%", 
-        justifyContent: "space-between", 
-        flexDirection: "row", 
+        width: "76.5%",
+        justifyContent: "space-between",
+        flexDirection: "row",
         alignItems: "center"
     },
-    listRowInfoContent: { 
-        width: "83%", 
-        paddingBottom: 10, 
+    listRowInfoContent: {
+        width: "83%",
+        paddingBottom: 10,
         marginTop: 10
     },
     listUserNameFont: {
-        fontWeight: "600", 
+        fontWeight: "600",
         color: "black"
     },
     listUserIdFont: {
-        paddingVertical: 3, 
-        paddingHorizontal: 7, 
-        backgroundColor: "#d3d3d3", 
-        color: "#fff", 
+        paddingVertical: 3,
+        paddingHorizontal: 7,
+        backgroundColor: "#d3d3d3",
+        color: "#fff",
         borderRadius: 100
+    },
+    footerUsersListContainer: {
+        width: "100%",
+        alignItems: "center"
+    },
+    footerUsersListBtn: {
+        backgroundColor: "black",
+        marginVertical: 5,
+        paddingHorizontal: 14,
+        paddingVertical: 10,
+        borderRadius: 5
+    },
+    footerUsersListBtnFont: {
+        color: "#fff"
+    },
+    loadingUsersContainer: {
+        marginTop: 20
+    },
+    loadingUsersFont: {
+        fontWeight: "600"
+    },
+    loadingIndicator: {
+        marginTop: 10
     }
 });
 
